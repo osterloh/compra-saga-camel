3060013abbadcfb74db14b648c3542b4ea958c4a
Johnatan Osterloh <johnatanosterloh@gmail.com>
2025-01-21 21:38:10 -0300

    Configuração da saga camel

---
+++ src/main/java/com/osterloh/SagaRoute.java
@@ -0,0 +1,58 @@
+package com.osterloh;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.SagaPropagation;
+import org.apache.camel.saga.CamelSagaService;
+import org.apache.camel.saga.InMemorySagaService;
+
+@ApplicationScoped
+public class SagaRoute extends RouteBuilder {
+
+    @Inject
+    OrderService orderService;
+
+    @Inject
+    CreditService creditService;
+
+    @Override
+    public void configure() throws Exception {
+    CamelSagaService sagaService = new InMemorySagaService(); // executa a saga em memoria
+        getContext().addService(sagaService);
+
+        //Saga
+        from("direct:saga").saga().propagation(SagaPropagation.REQUIRES_NEW).log("Starting the transaction")
+                .to("direct:newOrder").log("Creating new order")
+                .to("direct:newOrderValue").log("Reserving the credit")
+                .to("direct:finish").log("Success!");
+
+        //Order service
+        from("direct:newOrder").saga().propagation(SagaPropagation.MANDATORY)
+                .compensation("direct:cancelOrder")
+                .transform().header(Exchange.SAGA_LONG_RUNNING_ACTION)
+                .bean(orderService, "newOrder").log("Order ${body} created");
+        from("direct:cancelOrder")
+                .transform().header(Exchange.SAGA_LONG_RUNNING_ACTION)
+                .bean(orderService, "cancelOrder").log("Order ${body} canceled");
+
+        //Credit service
+        from("direct:newOrderValue").saga().propagation(SagaPropagation.MANDATORY)
+                .compensation("direct:cancelOrderValue")
+                .transform().header(Exchange.SAGA_LONG_RUNNING_ACTION)
+                .bean(creditService, "newOrderValue")
+                .log("Credit of order ${heder.orderId} in value of ${header.value} reserved to the saga ${body}");
+
+        from("direct:cancelOrderValue")
+                .transform().header(Exchange.SAGA_LONG_RUNNING_ACTION)
+                .bean(creditService, "cancelOrderValue")
+                .log("Credit of order ${heder.orderId} in value of ${header.value} reserved to the saga ${body}");
+
+        //Finished
+        from("finish").saga().propagation(SagaPropagation.MANDATORY)
+                .choice()
+                .when(header("fail").isEqualTo(true)).to("")
+                .end();
+    }
+}
